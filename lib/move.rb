8ad875a5ba84b73926be012e830c69657fba3b6e
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-06-10 03:48:39 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,7 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(board, input, token = "X")
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def move(array, index, value = "X")
+  array[index] = value
 end
-  
